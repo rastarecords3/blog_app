f2fa6e76f3e20cfccd8c39a1072b0929b49a113e
rastarecords3 <rastarecords3@gmail.com>
2025-11-03 14:42:39 +0000

    destroy article added, and link to go back to index from edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,12 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -32,6 +36,15 @@ class ArticlesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+
 
   private
   def article_params
